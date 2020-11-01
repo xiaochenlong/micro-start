6a8a71d3c98c8bbe4436cb43f643b4137e810944
肖臣龙 <xiaochenlong@ishanggang.com>
2020-11-01 19:17:21 +0800

    dict

---
+++ micro-start-dict/src/main/java/vip/corejava/DictApplication.java
@@ -0,0 +1,27 @@
+package vip.corejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author xcl
+ * @version 2020/11/1
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DictApplication {
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate loadbalancedRestTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DictApplication.class, args);
+    }
+}
